0c24472867009d7680444aa042cbff2283167562
Ellison Yasukochi <smellison@gmail.com>
2018-05-11 23:31:10 -0700

    Added espressif SD card sample as a test, to write to micro SD over SPI, seems to work alright. Need to write sensor data out to file, periodically.

--- main/main.cpp
+++ main/main.cpp
@@ -1,20 +1,17 @@
-/*
- * 1. Open up the project properties
- * 2. Visit C/C++ General > Preprocessor Include Paths, Macros, etc
- * 3. Select the Providers tab
- * 4. Check the box for "CDT GCC Built-in Compiler Settings"
- * 5. Set the compiler spec command to "xtensa-esp32-elf-gcc ${FLAGS} -E -P -v -dD "${INPUTS}""
- * 6. Rebuild the index
-*/
-
 #include <esp_log.h>
-#include <string>
+#include <string.h>
 #include "sdkconfig.h"
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/uart.h"
 #include "soc/rtc.h"
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "esp_vfs_fat.h"
+#include "driver/sdmmc_host.h"
+#include "driver/sdspi_host.h"
+
 
 
 static char tag[]="collision-detection";
@@ -28,8 +25,21 @@ static char tag[]="collision-detection";
 
 extern "C" {
 	#include "dac-cosine.h"
+	#include "sdmmc_cmd.h"
 	void app_main(void);
 }
+
+
+
+
+// Pin mapping when using SPI mode.
+// With this mapping, SD card can be used both in SPI and 1-line SD mode.
+// Note that a pull-up on CS line is required in SD mode.
+#define PIN_NUM_MISO GPIO_NUM_2
+#define PIN_NUM_MOSI GPIO_NUM_15
+#define PIN_NUM_CLK  GPIO_NUM_14
+#define PIN_NUM_CS   GPIO_NUM_13
+
 
 /* Declare global sine waveform parameters
  * so they may be then accessed and changed from debugger
@@ -129,7 +139,96 @@ static void sound_task(void *arg) {
 
 void app_main(void)
 {
-	xTaskCreate(sound_task, "dactask", 1024*3, NULL, 10, NULL);
+//	xTaskCreate(sound_task, "dactask", 1024*3, NULL, 10, NULL);
 //	xTaskCreate(detect_task, "uart_echo_task", 1024, NULL, 10, NULL);
+
+    ESP_LOGI(tag, "Using SPI peripheral");
+
+    sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+    sdspi_slot_config_t slot_config = SDSPI_SLOT_CONFIG_DEFAULT();
+    slot_config.gpio_miso = PIN_NUM_MISO;
+    slot_config.gpio_mosi = PIN_NUM_MOSI;
+    slot_config.gpio_sck  = PIN_NUM_CLK;
+    slot_config.gpio_cs   = PIN_NUM_CS;
+    // This initializes the slot without card detect (CD) and write protect (WP) signals.
+    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+
+
+    // Options for mounting the filesystem.
+    // If format_if_mount_failed is set to true, SD card will be partitioned and
+    // formatted in case when mounting fails.
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+        .format_if_mount_failed = false,
+        .max_files = 5,
+        .allocation_unit_size = 16 * 1024
+    };
+
+    // Use settings defined above to initialize SD card and mount FAT filesystem.
+    // Note: esp_vfs_fat_sdmmc_mount is an all-in-one convenience function.
+    // Please check its source code and implement error recovery when developing
+    // production applications.
+    sdmmc_card_t* card;
+    esp_err_t ret = esp_vfs_fat_sdmmc_mount("/sdcard", &host, &slot_config, &mount_config, &card);
+
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(tag, "Failed to mount filesystem. "
+                "If you want the card to be formatted, set format_if_mount_failed = true.");
+        } else {
+            ESP_LOGE(tag, "Failed to initialize the card (%s). "
+                "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
+        }
+        return;
+    }
+
+    // Card has been initialized, print its properties
+    sdmmc_card_print_info(stdout, card);
+
+    // Use POSIX and C standard library functions to work with files.
+    // First create a file.
+    ESP_LOGI(tag, "Opening file");
+    FILE* f = fopen("/sdcard/hello.txt", "w");
+    if (f == NULL) {
+        ESP_LOGE(tag, "Failed to open file for writing");
+        return;
+    }
+    fprintf(f, "Hello %s!\n", card->cid.name);
+    fclose(f);
+    ESP_LOGI(tag, "File written");
+
+    // Check if destination file exists before renaming
+    struct stat st;
+    if (stat("/sdcard/foo.txt", &st) == 0) {
+        // Delete it if it exists
+        unlink("/sdcard/foo.txt");
+    }
+
+    // Rename original file
+    ESP_LOGI(tag, "Renaming file");
+    if (rename("/sdcard/hello.txt", "/sdcard/foo.txt") != 0) {
+        ESP_LOGE(tag, "Rename failed");
+        return;
+    }
+
+    // Open renamed file for reading
+    ESP_LOGI(tag, "Reading file");
+    f = fopen("/sdcard/foo.txt", "r");
+    if (f == NULL) {
+        ESP_LOGE(tag, "Failed to open file for reading");
+        return;
+    }
+    char line[64];
+    fgets(line, sizeof(line), f);
+    fclose(f);
+    // strip newline
+    char* pos = strchr(line, '\n');
+    if (pos) {
+        *pos = '\0';
+    }
+    ESP_LOGI(tag, "Read from file: '%s'", line);
+
+    // All done, unmount partition and disable SDMMC or SPI peripheral
+    esp_vfs_fat_sdmmc_unmount();
+    ESP_LOGI(tag, "Card unmounted");
 }
 
