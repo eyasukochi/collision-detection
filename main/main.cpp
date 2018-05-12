73744e8ad36f6801d7380355987575853bd9c09a
Ellison Yasukochi <smellison@gmail.com>
2018-05-12 15:58:03 -0700

    Got detector writing to queue that is consumed by file writer, which then generates files on an SD card for later analysis.

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,6 @@
 #include <esp_log.h>
 #include <string.h>
+#include <string>
 #include "sdkconfig.h"
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
@@ -30,7 +31,7 @@ extern "C" {
 }
 
 
-
+static QueueHandle_t data_logging = NULL;
 
 // Pin mapping when using SPI mode.
 // With this mapping, SD card can be used both in SPI and 1-line SD mode.
@@ -77,9 +78,16 @@ static void detect_task(void *arg)
 
     uint8_t frames [9];
     uint8_t* data = (uint8_t*) malloc(1);
-    int check;
-    int dist;
-    int strength;
+    uint32_t check;
+    uint32_t dist;
+    uint32_t strength;
+
+    if (data_logging == NULL){
+    	ESP_LOGE(tag, "data_logging queue was null");
+    } else {
+    	ESP_LOGI(tag, "data_logging was instantiated");
+//    	data_logging->
+    }
 
 	while (1) {
 
@@ -98,6 +106,11 @@ static void detect_task(void *arg)
 						dist=frames[2]+frames[3]*256;//calculate distance value
 						strength=frames[4]+frames[5]*256;//calculate signal strength value
 						ESP_LOGI(tag, "dist = %d    strength = %d", dist, strength);
+						xQueueSendToBack(data_logging, &dist, portMAX_DELAY); // Values are copied out of the dist addr into the queue storage area
+					}
+					else
+					{
+						ESP_LOGW(tag, "skipping value due to bad hash");
 					}
 				}
 			}
@@ -109,6 +122,33 @@ static void detect_task(void *arg)
 		vTaskDelay(10/portTICK_RATE_MS);
 	}
 	free(data);
+}
+
+static void dumb_producer(void *arg) {
+	uint32_t constval = 100;
+	if (data_logging == NULL ){
+		ESP_LOGW(tag, "data_logging queue was null!!");
+	} else {
+		ESP_LOGI(tag, "data_logging queue has been created");
+	}
+	while(1) {
+		ESP_LOGI(tag, "Trying to write onto queue");
+		xQueueSendFromISR(data_logging, &constval, NULL);
+		vTaskDelay(10/portTICK_RATE_MS);
+	}
+}
+
+static void dumb_receiver(void *arg) {
+	uint32_t value;
+	if (data_logging == NULL ){
+		ESP_LOGW(tag, "data_logging queue was null!!");
+	} else {
+		ESP_LOGI(tag, "data_logging queue has been created");
+	}
+	while(1) {
+		xQueueReceive(data_logging, &value, portMAX_DELAY);
+		ESP_LOGI(tag, "Got back %d", value);
+	}
 }
 
 static void sound_task(void *arg) {
@@ -137,98 +177,164 @@ static void sound_task(void *arg) {
 		}
 }
 
+static void file_writer_task(void *arg) {
+	ESP_LOGI(tag, "Using SPI peripheral");
+
+	sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+	sdspi_slot_config_t slot_config = SDSPI_SLOT_CONFIG_DEFAULT();
+	slot_config.gpio_miso = PIN_NUM_MISO;
+	slot_config.gpio_mosi = PIN_NUM_MOSI;
+	slot_config.gpio_sck  = PIN_NUM_CLK;
+	slot_config.gpio_cs   = PIN_NUM_CS;
+	// This initializes the slot without card detect (CD) and write protect (WP) signals.
+	// Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+
+
+	// Options for mounting the filesystem.
+	// If format_if_mount_failed is set to true, SD card will be partitioned and
+	// formatted in case when mounting fails.
+	esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+		.format_if_mount_failed = false,
+		.max_files = 5,
+		.allocation_unit_size = 16 * 1024
+	};
+
+	// Use settings defined above to initialize SD card and mount FAT filesystem.
+	// Note: esp_vfs_fat_sdmmc_mount is an all-in-one convenience function.
+	// Please check its source code and implement error recovery when developing
+	// production applications.
+	sdmmc_card_t* card;
+	esp_err_t ret = esp_vfs_fat_sdmmc_mount("/sdcard", &host, &slot_config, &mount_config, &card);
+
+	if (ret != ESP_OK) {
+		if (ret == ESP_FAIL) {
+			ESP_LOGE(tag, "Failed to mount filesystem. "
+				"If you want the card to be formatted, set format_if_mount_failed = true.");
+		} else {
+			ESP_LOGE(tag, "Failed to initialize the card (%s). "
+				"Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
+		}
+		return;
+	}
+
+	// Card has been initialized, print its properties
+	sdmmc_card_print_info(stdout, card);
+	// TODO at this point, should read from a queue and when a certain buffer size has been reached, write a new file with all of those data points
+	// Qs: what is that buffer size? Or can I just leave the file open as I consume N values from the queue.
+	// Definitely have to write periodically because there will not be a shutdown op.
+
+	//TODO need to detect existing files and create incremental folder per "on" cycle. basically per time car was turned on.
+	//Then use esp_log_timestamp() to create per-cycle-unique file names
+
+	//convert from dir name to int, find the largest int, and then increment by one to create new dir
+//	    strtoimax("1", NULL, 10);
+
+	int values_to_write = 60000; // should be roughly once a minute
+	while(1) {
+
+		ESP_LOGI(tag, "Opening file");
+
+//	    	uint32_t now = esp_log_timestamp();
+		FILE* f = fopen("/sdcard/hello.txt", "w");
+		if (f == NULL) {
+			ESP_LOGE(tag, "Failed to open file for writing");
+			return;
+		}
+
+		uint32_t value;
+		for (int i = 0; i< values_to_write; i++) {
+			xQueueReceive(data_logging, &value, portMAX_DELAY);
+			fprintf(f, "%d,", value);
+		}
+		fclose(f);
+	}
+
+	// All done, unmount partition and disable SDMMC or SPI peripheral
+	//UNCLEAR HOW THIS COULD BE REACHED?
+//	esp_vfs_fat_sdmmc_unmount();
+//	ESP_LOGI(tag, "Card unmounted");
+}
+
 void app_main(void)
 {
+	data_logging = xQueueCreate(100, sizeof(uint32_t)); //TODO is this uint32?
+
+
+//	xTaskCreate(dumb_producer, "dumb_producer", 2048, NULL, 10, NULL);
+//	xTaskCreate(dumb_receiver, "dumb_receiver", 2048, NULL, 10, NULL);
+
+	ESP_LOGI(tag, "Using SPI peripheral");
+
+	sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+	sdspi_slot_config_t slot_config = SDSPI_SLOT_CONFIG_DEFAULT();
+	slot_config.gpio_miso = PIN_NUM_MISO;
+	slot_config.gpio_mosi = PIN_NUM_MOSI;
+	slot_config.gpio_sck  = PIN_NUM_CLK;
+	slot_config.gpio_cs   = PIN_NUM_CS;
+	// This initializes the slot without card detect (CD) and write protect (WP) signals.
+	// Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+
+
+	// Options for mounting the filesystem.
+	// If format_if_mount_failed is set to true, SD card will be partitioned and
+	// formatted in case when mounting fails.
+	esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+		.format_if_mount_failed = false,
+		.max_files = 5,
+		.allocation_unit_size = 16 * 1024
+	};
+
+	// Use settings defined above to initialize SD card and mount FAT filesystem.
+	// Note: esp_vfs_fat_sdmmc_mount is an all-in-one convenience function.
+	// Please check its source code and implement error recovery when developing
+	// production applications.
+	sdmmc_card_t* card;
+	esp_err_t ret = esp_vfs_fat_sdmmc_mount("/sdcard", &host, &slot_config, &mount_config, &card);
+
+	if (ret != ESP_OK) {
+		if (ret == ESP_FAIL) {
+			ESP_LOGE(tag, "Failed to mount filesystem. "
+				"If you want the card to be formatted, set format_if_mount_failed = true.");
+		} else {
+			ESP_LOGE(tag, "Failed to initialize the card (%s). "
+				"Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
+		}
+		return;
+	}
+
+	// Card has been initialized, print its properties
+//	sdmmc_card_print_info(stdout, card);
+
+	// create detector later because of how long sd initalization takes...
+	xTaskCreate(detect_task, "detector_task", 2*1024, NULL, 10, NULL);
+
+	int values_to_write = 1000;
+	while(1) {
+
+		ESP_LOGW(tag, "Opening file");
+
+		uint32_t now = esp_log_timestamp();
+		char fileName[50];
+		sprintf(fileName, "/sdcard/%d.txt", now);
+		FILE* f = fopen(fileName, "w");
+		if (f == NULL) {
+			ESP_LOGE(tag, "Failed to open file for writing");
+			return;
+		}
+
+		int value;
+		for (int i = 0; i< values_to_write; i++) {
+			xQueueReceive(data_logging, &value, portMAX_DELAY);
+			fprintf(f, "%d,", value);
+		}
+		fclose(f);
+	}
+
 //	xTaskCreate(sound_task, "dactask", 1024*3, NULL, 10, NULL);
-//	xTaskCreate(detect_task, "uart_echo_task", 1024, NULL, 10, NULL);
-
-    ESP_LOGI(tag, "Using SPI peripheral");
-
-    sdmmc_host_t host = SDSPI_HOST_DEFAULT();
-    sdspi_slot_config_t slot_config = SDSPI_SLOT_CONFIG_DEFAULT();
-    slot_config.gpio_miso = PIN_NUM_MISO;
-    slot_config.gpio_mosi = PIN_NUM_MOSI;
-    slot_config.gpio_sck  = PIN_NUM_CLK;
-    slot_config.gpio_cs   = PIN_NUM_CS;
-    // This initializes the slot without card detect (CD) and write protect (WP) signals.
-    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+//	xTaskCreate(file_writer_task, "sd_write_task", 3*1024, NULL, 10, NULL);
 
 
-    // Options for mounting the filesystem.
-    // If format_if_mount_failed is set to true, SD card will be partitioned and
-    // formatted in case when mounting fails.
-    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
-        .format_if_mount_failed = false,
-        .max_files = 5,
-        .allocation_unit_size = 16 * 1024
-    };
 
-    // Use settings defined above to initialize SD card and mount FAT filesystem.
-    // Note: esp_vfs_fat_sdmmc_mount is an all-in-one convenience function.
-    // Please check its source code and implement error recovery when developing
-    // production applications.
-    sdmmc_card_t* card;
-    esp_err_t ret = esp_vfs_fat_sdmmc_mount("/sdcard", &host, &slot_config, &mount_config, &card);
 
-    if (ret != ESP_OK) {
-        if (ret == ESP_FAIL) {
-            ESP_LOGE(tag, "Failed to mount filesystem. "
-                "If you want the card to be formatted, set format_if_mount_failed = true.");
-        } else {
-            ESP_LOGE(tag, "Failed to initialize the card (%s). "
-                "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
-        }
-        return;
-    }
-
-    // Card has been initialized, print its properties
-    sdmmc_card_print_info(stdout, card);
-
-    // Use POSIX and C standard library functions to work with files.
-    // First create a file.
-    ESP_LOGI(tag, "Opening file");
-    FILE* f = fopen("/sdcard/hello.txt", "w");
-    if (f == NULL) {
-        ESP_LOGE(tag, "Failed to open file for writing");
-        return;
-    }
-    fprintf(f, "Hello %s!\n", card->cid.name);
-    fclose(f);
-    ESP_LOGI(tag, "File written");
-
-    // Check if destination file exists before renaming
-    struct stat st;
-    if (stat("/sdcard/foo.txt", &st) == 0) {
-        // Delete it if it exists
-        unlink("/sdcard/foo.txt");
-    }
-
-    // Rename original file
-    ESP_LOGI(tag, "Renaming file");
-    if (rename("/sdcard/hello.txt", "/sdcard/foo.txt") != 0) {
-        ESP_LOGE(tag, "Rename failed");
-        return;
-    }
-
-    // Open renamed file for reading
-    ESP_LOGI(tag, "Reading file");
-    f = fopen("/sdcard/foo.txt", "r");
-    if (f == NULL) {
-        ESP_LOGE(tag, "Failed to open file for reading");
-        return;
-    }
-    char line[64];
-    fgets(line, sizeof(line), f);
-    fclose(f);
-    // strip newline
-    char* pos = strchr(line, '\n');
-    if (pos) {
-        *pos = '\0';
-    }
-    ESP_LOGI(tag, "Read from file: '%s'", line);
-
-    // All done, unmount partition and disable SDMMC or SPI peripheral
-    esp_vfs_fat_sdmmc_unmount();
-    ESP_LOGI(tag, "Card unmounted");
 }
 
